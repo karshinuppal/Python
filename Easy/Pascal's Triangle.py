2cb30473ada0e5d50b107993dbb4d832e49c6155
Karshin Uppal <112567011+karshinuppal@users.noreply.github.com>
2023-09-16 01:54:36 +0530

    Update Pascal's Triangle.py

--- Easy/Pascal's Triangle.py
+++ Easy/Pascal's Triangle.py
@@ -1,14 +1,14 @@
-Problem:
-Given an integer numRows, return the first numRows of Pascal's triangle.
+# Problem:
+# Given an integer numRows, return the first numRows of Pascal's triangle.
 
-In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+# In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
-Example 1:
+# Example 1:
 
-Input: numRows = 5
-Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+# Input: numRows = 5
+# Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
-Solution:
+# Solution:
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
